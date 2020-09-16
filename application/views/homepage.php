722f9131b35a950c4f7da3ee5c719111e82bbe3e
Grace Firgia <gracellata@gmail.com>
2020-09-16 13:59:53 +0700

    Initial Commit

---
+++ application/views/homepage.php
@@ -0,0 +1,110 @@
+<!-- start navbar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+	<div class="container">
+		<a class="navbar-brand" href="#">JQ EMPIRE</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav ml-auto">
+				<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="#">Pricing</a>
+				<a class="nav-item nav-link" href="#">Features</a>
+				<a class="nav-item nav-link " href="#">About</a>
+				<a class="nav-item btn btn-primary tombol" href="#">JOIN US</a>
+			</div>
+		</div>
+	</div>
+</nav>
+<!-- end navbar -->
+
+<!-- start jumbotron -->
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">
+			Get work done <span>faster</span> <br>
+			and <span>better</span> with us
+		</h1>
+		<a href="" class="btn btn-primary tombol">Our Work</a>
+	</div>
+</div>
+<!-- end jumbotron -->
+
+<!-- start container -->
+<div class="container">
+	<!--start info panel -->
+	<div class="row justify-content-center">
+		<div class="col-10 info-panel">
+			<div class="row">
+				<div class="col-lg">
+					<img src="assets/img/employee.png" alt="employee" class="float-left">
+					<h4>24 HOURS</h4>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div class="col-lg">
+					<img src="assets/img/hires.png" alt="employee" class="float-left">
+					<h4>HIGH-RES</h4>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div class="col-lg">
+					<img src="assets/img/security.png" alt="employee" class="float-left">
+					<h4>SECURITY</h4>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end info panel-->
+
+	<!-- start working space -->
+	<div class="row working-space">
+		<div class="col-lg-6">
+			<img src="assets/img/workingspace.png" alt="workingspace" class="img-fluid">
+		</div>
+		<div class="col-lg-5">
+			<h3>you <span>work</span> like <br> at <span>home</span></h3>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque, incidunt.</p>
+			<a href="" class="btn btn-primary tombol">Gallery</a>
+		</div>
+	</div>
+	<!-- end working space -->
+
+	<!-- start testimonial -->
+	<section class="testimonial">
+		<div class="row justify-content-center">
+			<div class="col-lg-8">
+				<h5>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, perspiciatis."</h5>
+			</div>
+		</div>
+
+		<div class="row justify-content-center">
+			<div class="col-lg-6 justify-content-center d-flex">
+				<figure class="figure">
+					<img src="assets/img/img2.png" class="figure-img img-fluid rounded-circle" alt="testi 1">
+				</figure>
+
+				<figure class="figure">
+					<img src="assets/img/img3.png" class="figure-img img-fluid rounded-circle utama" alt="testi 2">
+					<figcaption class="figure-caption">
+						<h5>QUINN</h5>
+						<p>Manager</p>
+					</figcaption>
+				</figure>
+
+				<figure class="figure">
+					<img src="assets/img/img1.png" class="figure-img img-fluid rounded-circle" alt="testi 3">
+				</figure>
+			</div>
+		</div>
+	</section>
+	<!-- end testimonial -->
+
+	<!-- start footer -->
+	<div class="row footer">
+		<div class="col text-center">
+			<p>&copy; 2020 All Rights Reserved by JQ Empire.</p>
+		</div>
+	</div>
+	<!-- endfooter -->
+</div>
+<!-- end container -->
